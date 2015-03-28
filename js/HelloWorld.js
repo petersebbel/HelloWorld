3ba0f0ead33c39ec1cefabd58b94d33d92324184
petersebbel <peter.sebbel@gmail.com>
2015-03-28 08:21:08 -0400

    First KO Example

---
+++ js/HelloWorld.js
@@ -0,0 +1,8 @@
+// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
+function AppViewModel() {
+    this.firstName = ko.observable("Bert");
+    this.lastName = ko.observable("Bertington");
+    this.fullName = ko.computed(function() {
+        return this.firstName() + " " + this.lastName();    
+    }, this);
+}
