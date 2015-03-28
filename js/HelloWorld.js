d3a20a480f7cf6fe2435c13cb26ae468e5f05084
petersebbel <peter.sebbel@gmail.com>
2015-03-28 19:44:44 -0400

    Latest

--- js/HelloWorld.js
+++ js/HelloWorld.js
@@ -11,46 +11,26 @@ function AppViewModel() {
     this.validatedSeq = ko.computed(function() {
         return this.inputSeq().toUpperCase().replace(/[^ACGTNacgtn]/gi, "");    
     }, this);
-    //var helper = self.validatedSeq();
-    //self.formatedSeq = ko.computed(formatSequence(helper));
 
 
-self.formatedSeq = ko.computed(function  () {
 
+	self.formatedSeq = ko.pureComputed(function  () {
 		var retSeq = "";
-		for (var i = 0; i < self.validatedSeq().length; i+= 60) {
-			var h = i;
-			while (h < 10000) {
-				if (h < 1) { h = 1};
+		var vlen = self.validatedSeq().length;
+		for (var i = 0; i < vlen; i+= 60) {
+			for (var j = 0; j <  (vlen.toString().length - i.toString().length); j++) {
 				retSeq += " ";
-				h = h * 10;
 			}
-			retSeq += i + " " + self.validatedSeq().substring(i, i+60) + " "  + ((i+60 < self.validatedSeq().length) ? i+60 : self.validatedSeq().length ) + "\n";
+			retSeq += i + " " + self.validatedSeq().substring(i, i+60) + " "  + ((i+60 < vlen) ? i+60 : vlen ) + "\n";
 		}
-	return retSeq;
+		return retSeq;
 	}, this);
     
-    
-
-    
-    this.doubleSeq = ko.computed(function() {
+	this.doubleSeq = ko.computed(function() {
         return this.validatedSeq() + this.validatedSeq();    
     }, this);
     
     
 }
 
-function formatSequence (seq) {
-	var tseq = seq.replace(/[^ACGTNacgtn]/gi, "");
-		var retSeq = "";
-		for (var i = 0; i < tseq.length; i+= 60) {
-			var h = i;
-			while (h < 10000) {
-				if (h < 1) { h = 1};
-				retSeq += " ";
-				h = h * 10;
-			}
-			retSeq += i + " " + tseq.substring(i, i+60) + " "  + ((i+60 < tseq.length) ? i+60 : tseq.length ) + "\n";
-		}
-	return retSeq;
-}
+
