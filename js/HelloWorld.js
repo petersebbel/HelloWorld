492d584af85ae6f5c3ce6c50bfa440b7b14bd7aa
petersebbel <peter.sebbel@gmail.com>
2015-03-29 17:35:56 -0400

    updates all over, unfinished

--- js/HelloWorld.js
+++ js/HelloWorld.js
@@ -1,8 +1,18 @@
+// Class to represent a scaffold
+function scaffold(name, start, end) {
+    var self = this;
+    self.name = name;
+    self.start = start;
+    self.end = end;
+}
+
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function AppViewModel() {
 	var self = this;
- 
+ 	self.scaffoldArray = ko.observableArray();    // Initially an empty array
     self.inputSeq = ko.observable("Enter your Sequence here");
+
+
     self.validatedSeq = ko.computed(function() {
         return self.inputSeq().toUpperCase().replace(/[^ACGTNacgtn]/gi, "");    
     }, self);
@@ -20,19 +30,27 @@ function AppViewModel() {
 	}, self);
     
 	self.doubleSeq = ko.computed(function() {
-        return this.validatedSeq() + this.validatedSeq();    
+		//self.scaffoldArray = findORFs(self.validatedSeq()).slice();
+		findORFs(self);
+
+        return self.validatedSeq() + self.validatedSeq();    
     }, self);
+
 }
 
 
-function findORFs(seq) {
+function findORFs(self) {
 	var regEx = /ATG((?!(TGA|TAG|TAA)).{3})*(NNN){1,}((?!(TGA|TAG|TAA)).{3})*(TAA|TGA|TAG)/gi;
 	var myArray;
-	while ((myArray = regEx.exec(str)) !== null) {
+	var retArray = [];
+	while ((myArray = regEx.exec(self.validatedSeq())) !== null) {
   		var msg = 'Found ' + myArray[0] + '. ';
   		msg += 'Next match starts at ' + regEx.lastIndex;
   		console.log(msg);
+  		retArray.push(myArray[0]);
+  		self.scaffoldArray.push(new scaffold(myArray[0], 1, regEx.lastIndex));
 	}
+	return retArray;
 
 }
 
